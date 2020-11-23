26bd682dcabf20e51664ffe67151dd573882723c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-11-23 16:00:27 +0100

    fix permission issues

--- scripts/air.sh
+++ scripts/air.sh
@@ -5,7 +5,7 @@ if [[ "$BUILD" -eq "1" ]]; then
     echo "Building..."
     GOOS=linux go build -o ./tmp/main ./src
   fi
-elif [[ "$RUN" -eq "1" ]] then
+elif [[ "$RUN" -eq "1" ]]; then
     if [[ "$TEST" -eq "1" ]] && [[ "$DEBUG" -eq "1" ]]; then
         echo "Debugging tests..."
         dlv test --listen=:2345 --headless=true --api-version=2 -p 1 -v ./...
