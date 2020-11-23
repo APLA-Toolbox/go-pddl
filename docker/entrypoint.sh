59ac35849ad285c9ab625ab1a565f3df4d4e7af1
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-11-23 16:05:34 +0100

    remove useless log

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-export BUILD_TIME=$(date '+%FT%T%:z')
-export BUILD_GIT=$(git rev-parse HEAD)
-
 go mod vendor
 go generate ./src
 
 echo TEST : "${TEST}"
 echo DEBUG : "${DEBUG}"
-echo TEST_DEBUG_PACKAGE : "${TEST_DEBUG_PACKAGE}"
-
 source .env
 
-air 
+air
